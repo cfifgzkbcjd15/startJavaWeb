9e4215d4e4b24017e63059a00a81a796780b1423
Александр Пялисов <cfifgzkbcjd@mail.ru>
2023-08-24 18:09:53 +0300

    start Webjava

---
+++ src/main/java/Social/Model/People.java
@@ -0,0 +1,12 @@
+package Social.Model;
+
+public class People
+{
+    public String Name;
+    public int Age;
+    public People(String name,int age){
+        Name=name;
+        Age=age;
+    }
+
+}
